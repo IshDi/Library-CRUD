2c519789e5ae38b90ca9833cbae5f93d33399450
Dinara Andrianova <ish.dinara@gmail.com>
2023-07-12 22:54:26 +0300

    add models

---
+++ src/main/java/ru/dinara/models/Person.java
@@ -0,0 +1,29 @@
+package ru.dinara.models;
+
+public class Person {
+    private String fullName;
+    private int yearOfBirth;
+
+    public Person() {}
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName() {
+        return fullName;
+    }
+
+    public void setName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
